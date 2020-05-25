106403b9f88909b037bb985120d72ff62c616b31
atkhamov <atkhamov@gmail.com>
2020-05-26 01:48:44 +0600

    third commit - todos.vue -> v-for adjusted

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,7 +1,7 @@
 // Start TEMPLATE
 <template>
    <div>
-      <div v-for-key="todo in todos">
+      <div v-bind:key="todo.id" v-for="todo in todos">
          <h3>{{todo.title}}</h3>
       </div>
    </div>
