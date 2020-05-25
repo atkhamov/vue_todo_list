d0c27723a0ac743c2bfde536b5dc36a73a491f81
atkhamov <atkhamov@gmail.com>
2020-05-26 01:37:10 +0600

    second commit - todos array added

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+// Start TEMPLATE
+<template>
+   <div>
+      <div v-for-key="todo in todos">
+         <h3>{{todo.title}}</h3>
+      </div>
+   </div>
+</template>
+
+// Start SCRIPT
+<script>
+export default {
+   name: "Todos",
+   props: ["todos"]
+}
+</script>
+
+// Start STYLE
+<style scoped>
+
+</style>
